d2344cf814dd992d786fed7883e84b25eed35343
Daimen Williams <daimenwill@gmail.com>
2017-05-03 22:08:25 -0400

    Started to add basic gameplay logic to desktop app, game starts and player img loads - player can move with WASD - app terminates upon exit

--- Project_Circu/core/src/com/mythmake/circu/Circu.java
+++ Project_Circu/core/src/com/mythmake/circu/Circu.java
@@ -2,32 +2,71 @@ package com.mythmake.circu;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
 
 public class Circu extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+	
+	private Texture redcharImg;
+	//private Texture charbulletImg;
+	//private Texture hexaminionImg;
+	private Rectangle redchar;
+	//private Rectangle charbullet;
+	//private Rectangle hexaminion;
+	
+	 private OrthographicCamera camera;
+	 private SpriteBatch batch;
+	
 	
 	@Override
 	public void create () {
+		
+		redcharImg = new Texture(Gdx.files.internal("redchar.png"));
+	//	charbulletImg = new Texture(Gdx.files.internal("charbullet.png"));
+		//hexaminionImg = new Texture(Gdx.files.internal("hexaminion.png"));
+		
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 800, 480);
+		
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		
+		redchar = new Rectangle();
+		redchar.width = 60;
+		redchar.height = 60;
+		redchar.x = (800/2-60/2);
+		redchar.y = (480/2-60/2);
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		Gdx.gl.glClearColor(0, 0, 0.2f, 1);
+	    Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+	    
+	    camera.update();
+	    batch.setProjectionMatrix(camera.combined);
+	    batch.begin();
+	    batch.draw(redcharImg, redchar.x, redchar.y);
+	    batch.end();
+	    
+	    if(Gdx.input.isKeyPressed(Input.Keys.A)) redchar.x -= 200 * Gdx.graphics.getDeltaTime();
+	    if(Gdx.input.isKeyPressed(Input.Keys.D)) redchar.x += 200 * Gdx.graphics.getDeltaTime();
+	    if(Gdx.input.isKeyPressed(Input.Keys.W)) redchar.y += 200 * Gdx.graphics.getDeltaTime();
+	    if(Gdx.input.isKeyPressed(Input.Keys.S)) redchar.y -= 200 * Gdx.graphics.getDeltaTime();
+	    
+	    if(redchar.x < 0) redchar.x = 0;
+	    if(redchar.x > 800 - 60) redchar.x = 800 - 60;
+	    if(redchar.y < 0) redchar.y = 0;
+	    if(redchar.y > 480 - 60) redchar.y = 480 - 60;
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+		
+		redcharImg.dispose();
+	    batch.dispose();
 	}
 }
