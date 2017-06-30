5d140627a93ec425a21715061be46e586de029cb
Daimen Williams <daimenwill@gmail.com>
2017-06-30 16:02:51 -0400

    fixed collision detection logic, assuming circular hitboxes for now

--- Project_Circu/core/src/com/mythmake/circu/CollisionMechanics.java
+++ Project_Circu/core/src/com/mythmake/circu/CollisionMechanics.java
@@ -19,6 +19,9 @@ public class CollisionMechanics {
 	float secondXRadius;
 	float firstYRadius;
 	float secondYRadius;
+	float deltaX;
+	float deltaY;
+	float distance;
 	Vector2 firstCenter = new Vector2();
 	Vector2 secondCenter = new Vector2();
 	boolean collision;
@@ -27,13 +30,22 @@ public class CollisionMechanics {
 	public boolean hasInteraction(Array<Rectangle> friendly, Array<Rectangle> hostile){
 		firstCenter = friendly.get(0).getCenter(firstCenter);
 		secondCenter = hostile.get(0).getCenter(secondCenter);
+		deltaX = Math.abs(firstCenter.x - secondCenter.x);
+		deltaY = Math.abs(firstCenter.y - secondCenter.y);
+		distance = (float) Math.pow(Math.pow(deltaX, 2) + Math.pow(deltaY, 2), 0.5);
 		firstXRadius = friendly.get(0).width/2;
 		secondXRadius = hostile.get(0).width/2;
-
-		if((Math.abs(firstCenter.x - secondCenter.x) <= (firstXRadius + secondXRadius)) ||
+		
+		//assuming circular hitboxes, the distance between midpoints upon contact will have to be
+		//equal to or less than the sum of both object's radii 
+		/*if((Math.abs(firstCenter.x - secondCenter.x) <= (firstXRadius + secondXRadius)) ||
 				(Math.abs(firstCenter.y - secondCenter.y) <= (firstYRadius + secondYRadius))	){
 			collision = true;
 			System.out.println("enemy collision occured!");
+		} */
+		if(distance <= (firstXRadius + secondXRadius)){
+			collision = true;
+			System.out.println("collision has occurred");
 		}
 		else{
 			collision = false;
